92db6226f0450c437b2f05ae967edf0401f4132a
HarshitKachhadiya <harshit.kachhadiya18815@marwadieducation.edu.in>
2024-09-24 14:32:32 +0530

    added home to navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { assets } from '../../assets/assets';
 
-const pages = ['Resources', 'Experience', 'Discussion Forum'];
+const pages = ['Home','Resources', 'Experience', 'Discussion Forum'];
 const settings = ['Profile','Logout'];
 
 const Navbar = () => {
