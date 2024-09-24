2312af5f38e8b6769bdccae14f76f2c268000109
HarshitKachhadiya <harshit.kachhadiya18815@marwadieducation.edu.in>
2024-09-24 14:29:37 +0530

    chaged path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Container } from '@mui/material'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
-    <Container>
-      <Navbar></Navbar>
-    </Container>
+   <Container>
+    <Home/>
+   </Container>
    
   )
 }
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from '@mui/material'
+import Navbar from '../../Components/Navbar/Navbar'
+
+
+const Home = () => {
+  return (
+    <div className='home' id='home'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
