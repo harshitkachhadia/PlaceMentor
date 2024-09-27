289d2d46f2eadc404ea35a2e68bba7312972e7c5
HarshitKachhadiya <harshit.kachhadiya18815@marwadieducation.edu.in>
2024-09-27 11:16:51 +0530

    footer updated

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -2,44 +2,132 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: 'black', color: 'gray', padding: '2rem 0 3rem 0'}}>
-      <div className="container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', flexWrap: 'wrap',marginBottom:'-20px' }}>
+    <footer style={{
+      backgroundColor: 'black',
+      color: 'gray',
+      padding: '8rem 0 2rem 0',
+    }}>
+      <div className="container" style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        flexWrap: 'wrap',
+        marginBottom: '-20px',
+      }}>
         
-        {/* Section 1: Interesting Line & Social Links */}
-        <div className="footer-section" style={{ flex: '1', margin: '1rem' }}>
-          <h3 style={{ color: '#18FFFF' }}>Stay Connected!</h3>
-          <p style={{ marginBottom: '1rem' }}>Join us and never miss an opportunity!</p>
+        <div className="footer-section" style={{
+          flex: '1',
+          margin: '2rem', 
+        }}>
+          <h3 style={{
+            color: '#18FFFF',
+            fontSize: '1.5rem', 
+            marginBottom: '1rem'
+          }}>
+            Stay Connected!
+          </h3>
+          <p style={{
+            marginBottom: '2rem', 
+          }}>
+            Join us and never miss an opportunity!
+          </p>
           <div className="social-icons">
-            <a href="https://linkedin.com" style={{ color: '#00FFFF', marginRight: '1rem'}}>
-              <i className="fab fa-linkedin fa-2x"></i>
+            <a href="https://linkedin.com" style={{
+              color: '#00FFFF',
+              marginRight: '2rem', 
+            }}>
+              <i className="fab fa-linkedin fa-2x"></i> 
             </a>
-            <a href="https://github.com" style={{ color: '#00FFFF', marginRight: '1rem' }}>
-              <i className="fab fa-github fa-2x"></i>
+            <a href="https://github.com" style={{
+              color: '#00FFFF',
+              marginRight: '2rem', 
+            }}>
+              <i className="fab fa-github fa-2x"></i> 
             </a>
           </div>
         </div>
         
-        {/* Section 2: Pages Links */}
-        <div className="footer-section" style={{ flex: '1', margin: '1rem' }}>
-          <h3 style={{ color: '#18FFFF' }}>Quick Links</h3>
-          <ul style={{ listStyleType: 'none', padding: 0  }}>
-            <li style={{marginBottom:'0.1rem' }} ><a href="/resources" style={{ color: '#FFFFFF'}}>Resources</a></li>
-            <li style={{marginBottom:'0.1rem' }}><a href="/experience" style={{ color: '#FFFFFF'}}>Experience</a></li>
-            <li><a href="/forum" style={{ color: '#FFFFFF'}}>Discussion Forum</a></li>
+        <div className="footer-section" style={{
+          flex: '1',
+          margin: '2rem', 
+        }}>
+          <h3 style={{
+            color: '#18FFFF',
+            fontSize: '1.5rem', 
+            marginBottom: '1rem'
+          }}>
+            Quick Links
+          </h3>
+          <ul style={{
+            listStyleType: 'none',
+            padding: 0,
+          }}>
+            <li style={{
+              marginBottom: '0.5rem', 
+            }}>
+              <a href="/resources" style={{
+                color: '#FFFFFF',
+                fontSize: '1.2rem',
+              }}>
+                Resources
+              </a>
+            </li>
+            <li style={{
+              marginBottom: '0.5rem', 
+            }}>
+              <a href="/experiences" style={{
+                color: '#FFFFFF',
+                fontSize: '1.2rem', 
+              }}>
+                Experiences
+              </a>
+            </li>
+            <li>
+              <a href="/forum" style={{
+                color: '#FFFFFF',
+                fontSize: '1.2rem', 
+              }}>
+                Discussion Forum
+              </a>
+            </li>
           </ul>
         </div>
         
-        {/* Section 3: Contact Us */}
-        <div className="footer-section" style={{ flex: '1', margin: '1rem' }}>
-          <h3 style={{ color: '#18FFFF' }}>Contact Us</h3>
-          <p>Email: info@placementor.com</p>
-          <p>Phone: +123 456 xxxx</p>
+        <div className="footer-section" style={{
+          flex: '1',
+          margin: '2rem', 
+        }}>
+          <h3 style={{
+            color: '#18FFFF',
+            fontSize: '1.5rem',
+            marginBottom: '1rem' 
+          }}>
+            Contact Us
+          </h3>
+          <p style={{
+            fontSize: '1.2rem', 
+          }}>
+            Email: info@placementor.com
+          </p>
+          <p style={{
+            fontSize: '1.2rem', 
+          }}>
+            Phone: +123 456 xxxx
+          </p>
         </div>
       </div>
       
-      {/* Copyright Section */}
-      <div style={{ textAlign: 'center', marginTop: '2rem', borderTop: '1px solid gray', paddingTop: '1rem' }}>
-        <p>© 2024 PlaceMentor. All Rights Reserved.</p>
+      <div style={{
+        textAlign: 'center',
+        marginTop: '4rem', 
+        borderTop: '1px solid gray',
+        paddingTop: '2rem', 
+      }}>
+        <p style={{
+          fontSize: '1.2rem', 
+        }}>
+          © 2024 PlaceMentor. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
